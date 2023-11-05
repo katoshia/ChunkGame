a29b8f192edbdd24959404d013ac3046c91ba88f
katoshia <katoshia@gmail.com>
2023-11-05 15:49:28 -0600

    Camera Set to Player

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -14,10 +14,36 @@ public class Chunk : MonoBehaviour
     List<Vector3> vertices = new List<Vector3>();
     List<int> triangles = new List<int>();
     List<Vector2> uvs = new List<Vector2>();
+
+    // bool for checking if face is touching another face.
+    bool[,,] coordMap = new bool[CoordinateData.chunkWidth, CoordinateData.chunkHeight, CoordinateData.chunkWidth];
     // Start is called before the first frame update
     void Start()
     {
-        for(int x = 0;x<CoordinateData.chunkWidth;x++)
+        PopulateCoordMap();
+        CreateChunk();
+        CreateMesh();
+    }
+    // populate the coordinate map for chunk
+    void PopulateCoordMap()
+    {
+        for (int x = 0; x < CoordinateData.chunkWidth; x++)
+        {
+            for (int y = 0; y < CoordinateData.chunkHeight; y++)
+            {
+                for (int z = 0; z < CoordinateData.chunkWidth; z++)
+                {
+                    coordMap[x, y, z] = true;
+                }
+
+            }
+        }
+    }
+    // create the chunk using the mesh
+    void CreateChunk()
+    {
+        // draw the chunk for the max size set
+        for (int x = 0; x < CoordinateData.chunkWidth; x++)
         {
             for (int y = 0; y < CoordinateData.chunkHeight; y++)
             {
@@ -28,11 +54,6 @@ public class Chunk : MonoBehaviour
 
             }
         }
-
-        // will move the voxel/block
-        //AddCoorDataToChunk(transform.position+transform.right);
-        CreateMesh();
-
     }
     void CreateMesh()
     {
@@ -44,19 +65,36 @@ public class Chunk : MonoBehaviour
         mesh.RecalculateNormals();
         meshFilter.mesh = mesh;
     }
+    // check coordniates for faces - return the coordinates
+    bool CheckCoord(Vector3 position)
+    {
+        int x = Mathf.FloorToInt(position.x);
+        int y = Mathf.FloorToInt(position.y);
+        int z = Mathf.FloorToInt(position.z);
+
+        if (x < 0 || x > CoordinateData.chunkWidth - 1 || y < 0||y>CoordinateData.chunkHeight-1 ||z<0||z>CoordinateData.chunkWidth-1)
+        {
+            return false;
+        }
+        return coordMap[x,y,z];
+    }
     void AddCoorDataToChunk(Vector3 position)
     {
         for (int j = 0; j < 6; j++)
         {
-            for (int i = 0; i < 6; i++)
+            if(!CheckCoord(position+CoordinateData.touchCheck[j]))
             {
-                int triangleIndex = CoordinateData.coordTris[j, i];
-                vertices.Add(CoordinateData.coordVerts[triangleIndex]+position);
+                for (int i = 0; i < 6; i++)
+                {
+                    int triangleIndex = CoordinateData.coordTris[j, i];
+                    vertices.Add(CoordinateData.coordVerts[triangleIndex] + position);
 
-                triangles.Add(vertexIndex);
-                uvs.Add(CoordinateData.coordUVS[i]);
-                vertexIndex++;
+                    triangles.Add(vertexIndex);
+                    uvs.Add(CoordinateData.coordUVS[i]);
+                    vertexIndex++;
+                }
             }
+
         }
     }
 
--- Assets/Scripts/CoordinateData.cs
+++ Assets/Scripts/CoordinateData.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// read only class for holding all data for use in other classes.
 public static class CoordinateData
 {
     public static readonly int chunkWidth = 5;
-    public static readonly int chunkHeight = 5;
+    public static readonly int chunkHeight = 1;
+    // coordinates of the block/voxel storage
     public static readonly Vector3[] coordVerts = new Vector3[8]
     {
        new Vector3(0f,0f,0f),
@@ -17,7 +19,17 @@ public static class CoordinateData
        new Vector3(1.0f,1.0f,1.0f),
        new Vector3(0f,1.0f,1.0f)
    };
-
+    // check for faces touching
+    public static readonly Vector3[] touchCheck = new Vector3[6]
+    {
+       new Vector3(0f,0f,-1f), // check backface
+       new Vector3(0f,0f,1f), // check front face
+       new Vector3(0f,1f,0f), // check top
+       new Vector3(0f,-1f,0f),// check bottom
+       new Vector3(-1f,0f,0f),// check left
+       new Vector3(1f,0f,0f) // check right
+    };
+    // coordinates of the two triables that make up each face of the block(2 triables per face)
     public static readonly int[,] coordTris = new int[6,6]
     {
         {0,3,1,1,3,2 }, // back face
@@ -27,7 +39,7 @@ public static class CoordinateData
         {4,7,0,0,7,3 },// left face
         {1,2,5,5,2,6 }//right face
     };
-
+    // storage for the material coordinates to the faces.
     public static readonly Vector2[] coordUVS = new Vector2[6]
     {
         new Vector2(0.0f,0.0f),
@@ -37,4 +49,5 @@ public static class CoordinateData
         new Vector2(0.0f,1.0f),
         new Vector2(1.0f,1.0f)
     };
+
 }
