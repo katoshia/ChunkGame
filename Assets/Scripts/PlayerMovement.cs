3523c1cc257afd9441c4e4bc1c6ac832cfedea73
katoshia <katoshia@gmail.com>
2023-11-05 15:58:51 -0600

    Camera attached to player object
    Partial Player Movement to test camera follows player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 7f;
+
+    private float dirX = 0f;
+    private BoxCollider coll;
+    private Rigidbody rb;
+
+    private void Start()
     {
-        Debug.Log("Test Test");
+        rb = GetComponent<Rigidbody>();
+        coll = GetComponent<BoxCollider>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        dirX = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+
     }
 }
